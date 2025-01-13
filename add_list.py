e47e432ace65f482e6ba2e5efe3318b79fad15ee
IZerdiy <i.zherdiy@mail.ru>
2025-01-13 13:43:02 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,8 @@
+header1 = input('Введите название заметки: ')
+header2 = input('Введите название заметки: ')
+header3 = input('Введите название заметки: ')
+header_list = [header1, header2, header3]
+print('Первый заголовок: ', header1)
+print('Второй заголовок: ', header2)
+print('Третий заголовок: ', header3)
+print('Список заголовков: ', header_list)
