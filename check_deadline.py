2372822594f9fce9f78ee2a454ba64d75a4305e3
IZerdiy <i.zherdiy@mail.ru>
2025-01-14 20:36:47 +0300

    Add files via upload

---
+++ check_deadline.py
@@ -0,0 +1,18 @@
+import datetime
+
+current_date = datetime.date.today() # Получение даты в данный момент
+deadline = input('Введите дату дедлайна в формате ДД-ММ-ГГ: ') # Пользователь вводит дату дату дедлайна
+deadline_date = deadline
+try: # Оповещение в случае ввода неверного формата даты
+    deadline_date = datetime.date(int(deadline.split('-')[2]), int(deadline.split('-')[1]), int(deadline.split('-')[0]))
+except ValueError:
+    print('Неверный формат даты. Пожалуйста, введите дату в формате ДД-ММ-ГГ')
+except IndexError:
+    print('Неверный формат даты. Пожалуйста, введите дату в формате ДД-ММ-ГГ')
+if deadline_date < current_date : # Оповещает сколько осталось до дедлайна или когда он прошел
+    print(f'Внимание! Дедлайн истек! {(current_date - deadline_date).days} дней назад')
+elif current_date < deadline_date:
+    print(f'Внимание! До дедлайна осталось {(deadline_date - current_date).days} дней')
+else :
+    print('Дедлайн наступил сегодня')
+
